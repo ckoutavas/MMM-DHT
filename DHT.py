e69436cd1549d5163baf247a6b8182508cb0cf44
chriskoutavas <christopher.koutavas@gmail.com>
2023-05-07 14:46:38 -0500

    updated get_data function

--- DHT.py
+++ DHT.py
@@ -4,19 +4,13 @@ import board
 import adafruit_dht
 
 
-def get_data(dht: adafruit_dht) -> Tuple[float, float]:
-    try:
-        # get the temp and humidity
-        # temperature = round(dht.temperature, 1)
+def get_data():
+    dht = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+    if dht.humidity and dht.humidity not in [None, 'None']:
         humidity = round(dht.humidity, 1)
-        if humidity:
-            return humidity
-        else:
-            get_data(adafruit_dht.DHT22(board.D4, use_pulseio=False))
-    except:
-        time.sleep(2)
-        get_data(adafruit_dht.DHT22(board.D4, use_pulseio=False))
+        print(humidity)
+try:
+    get_data()
+except:
+    get_data()
 
-
-hum = get_data(adafruit_dht.DHT22(board.D4, use_pulseio=False))
-print(hum)
