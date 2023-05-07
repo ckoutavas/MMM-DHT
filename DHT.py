e3bc4260b7449dcac3635bc40fb0e21b07dffa13
chriskoutavas <christopher.koutavas@gmail.com>
2023-05-07 13:34:11 -0500

    Uploaded files

--- DHT.py
+++ DHT.py
@@ -4,13 +4,12 @@ import board
 import adafruit_dht
 
 
-def get_data(dht: adafruit_dht) -> Tuple[float, float, float]:
+def get_data(dht: adafruit_dht) -> Tuple[float, float]:
     try:
         # get the temp and humidity
-        temperature_c = round(dht.temperature, 1)
-        temperature_f = round(temperature_c * (9 / 5) + 32, 1)
+        temperature = round(dht.temperature, 1)
         humidity = round(dht.humidity, 1)
-        return temperature_c, temperature_f, humidity
+        return temperature, humidity
     except:
         time.sleep(2)
         get_data(adafruit_dht.DHT22(board.D4, use_pulseio=False))
---
+++ MMM-DHT.js
@@ -0,0 +1,55 @@
+Module.register("MMM-DHT", {
+	defaults: {
+	    tempUnit: "F",
+    	freq: 60000,
+    	highRH: 80,
+    	lowRH: 70,
+    	highColor: "red",
+    	lowColor: "yellow",
+    	midColor: "green",
+	},
+
+	start: function() {
+   		this.sendSocketNotification("get_dht");
+  	},
+
+  	getDom: function() {
+		var e = document.createElement("div")
+   		e.id = "pi_dht"
+		return e
+	},
+
+ 	 notificationReceived: function(notification, payload, sender) {
+	 	switch(notification) {
+      			case "DOM_OBJECTS_CREATED":
+        		var timer = setInterval(()=>{
+				this.sendSocketNotification("get_dht")
+        		}, this.config.freq)
+        		break
+    		}
+	},
+
+  	socketNotificationReceived: function(notification, payload) {
+		switch (notification) {
+			case "dht":
+			    var temp = payload[0]
+			    var humidity = payload[1]
+				var e = document.getElementById("pi_dht");
+				if (parseFloat(payload[1]) <= this.config.lowRH) {
+					e.style.color = this.config.lowColor;
+				} else if (parseFloat(payload) >= this.config.highRH) {
+					e.style.color = this.config.highColor;
+				} else {
+					e.style.color = this.config.midColor;
+				}
+
+				if (this.config.tempUnit === "C") {
+					temp = temp.toString() + "°C";
+				} else {
+					temp = (temp * (9/5) + 32).toFixed(1).toString() + "°F";
+				}
+				e.innerHTML =  temp + </ br> + humidity + "% RH";
+				break;
+		}
+  	},
+})
